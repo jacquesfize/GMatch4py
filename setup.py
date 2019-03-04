ea41e13aea5fd7c828811455dbe4039ccf374c44
Fize Jacques <jacques.fize@cirad.fr>
2019-03-04 17:18:10 +0100

    Add gensim to requirements

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-requirements=["numpy","networkx","scipy",'scikit-learn','tqdm','pandas',"joblib"]
+requirements=["numpy","networkx","scipy",'scikit-learn','tqdm','pandas',"joblib","gensim"]
 setup(
     name="GMatch4py",
     author="Jacques Fize",
